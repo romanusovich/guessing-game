3b42331a8e6406bdf1f9dc50fa167fcc298568a8
romanusovich1 <romanusovich1@gmail.com>
2023-02-27 17:00:34 +0300

    feat: guessing game pre work

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,29 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let L = this.min;
+        let R = this.max;
+        let m = Math.floor((L + R) / 2);
+        return m;
     }
 
     lower() {
-
+        let L = this.min;
+        let R = this.max;
+        let m = Math.floor((L + R) / 2);
+        this.max = m - 1;
     }
 
     greater() {
-
+        let L = this.min;
+        let R = this.max;
+        let m = Math.floor((L + R) / 2);
+        this.min = m + 1;
     }
 }
 
