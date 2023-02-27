daecdb3bd898afe6c15d2a2e1f784ee519b72d26
romanusovich <romanusovich1@gmail.com>
2023-02-27 19:47:55 +0300

    feat: guessing game complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -9,22 +9,22 @@ class GuessingGame {
     guess() {
         let L = this.min;
         let R = this.max;
-        let m = Math.floor((L + R) / 2);
+        let m = Math.ceil((L + R) / 2);
         return m;
     }
 
     lower() {
         let L = this.min;
         let R = this.max;
-        let m = Math.floor((L + R) / 2);
-        this.max = m - 1;
+        let m = Math.ceil((L + R) / 2);
+        this.max = m;
     }
 
     greater() {
         let L = this.min;
         let R = this.max;
-        let m = Math.floor((L + R) / 2);
-        this.min = m + 1;
+        let m = Math.ceil((L + R) / 2);
+        this.min = m;
     }
 }
 
